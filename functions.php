9d8a5d8bf7572a9d7043fc167560e90083866fcc
Vitaly <vitalykk1983@gmail.com>
2019-01-25 21:33:49 +0200

    Refactoring functions.php

--- functions.php
+++ functions.php
@@ -1,8 +1,12 @@
 <?php
 
+add_filter('show_admin_bar', '__return_false');
+
 add_action('wp_enqueue_scripts', 'test_media');
 
 function test_media()
 {
-    wp_enqueue_style('test_main', get_stylesheet_uri());
+    wp_enqueue_style('test-main', get_stylesheet_uri());
+
+    wp_enqueue_script('test-main-script', get_template_directory_uri() . '/assets/js/script.js');
 }
