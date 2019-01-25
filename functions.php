c167b0d3e5b6df49d9e7d7c5203d5b4cf4afcd6b
Vitaly <vitalykk1983@gmail.com>
2019-01-25 23:06:23 +0200

    Add interactive menu

--- functions.php
+++ functions.php
@@ -3,10 +3,16 @@
 add_filter('show_admin_bar', '__return_false');
 
 add_action('wp_enqueue_scripts', 'test_media');
+add_action('after_setup_theme', 'test_after_setup');
 
 function test_media()
 {
     wp_enqueue_style('test-main', get_stylesheet_uri());
 
     wp_enqueue_script('test-main-script', get_template_directory_uri() . '/assets/js/script.js');
+}
+
+function test_after_setup()
+{
+    register_nav_menu('top', 'My news Menu');
 }
--- header.php
+++ header.php
@@ -10,18 +10,20 @@
         <div class="wrapper">
             <header>
                 <div class="header-top clearfix">
-                    <a href="<?php home_url();
-        5 ?>" class="logo">Wood Design</a>
+                    <a href="<?php
+                    home_url();
+                    5
+                    ?>" class="logo">Wood Design</a>
                     <nav>
                         <div class="menu-button">MENU</div>
-                        <ul>
-                            <li><a href="#" class="active">Home</a></li>
-                            <li><a href="#">About</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
+                        <?php
+                        wp_nav_menu([
+                            'theme_location' => 'top',
+                            'container' => null
+                        ]);
+                        ?>
                     </nav>
+
                 </div>
                 <div  class="header-bottom">
                     <span>Wood Design is a modern web & graphic design studio in Europe. We create beautiful things for web and print. You can see our great work examples in <a href="#">Portfolio</a>. If you need a professional design services <a href="#">Contact</a> us. We would love to work with you.</span>
