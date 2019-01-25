d13bf33ffd70b36f771efeb319d440c6854ac713
Vitaly <vitalykk1983@gmail.com>
2019-01-26 00:03:52 +0200

    Add sidebar.php

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ add_filter('show_admin_bar', '__return_false');
 
 add_action('wp_enqueue_scripts', 'test_media');
 add_action('after_setup_theme', 'test_after_setup');
+add_action('widgets_init', 'test_widgets');
 
 function test_media()
 {
@@ -15,4 +16,13 @@ function test_media()
 function test_after_setup()
 {
     register_nav_menu('top', 'My news Menu');
+}
+
+function test_widgets()
+{
+    register_sidebar([
+        'name' => 'sidebar-right',
+        'id' => 'sidebar-right',
+        'description' => 'Right Column',
+    ]);
 }
--- index.php
+++ index.php
@@ -31,38 +31,8 @@
             <a href= "#">See Portfolio</a>
         </div>
     </main>
-    <aside>
-        <div class="tw-wrapper clearfix">
-            <div  class="tw-inner">
-                <div  class="tw-text">
-                    <span>Free Wood Design PSD Template. For more freebies and photoshop tutorials follow @webdesignfan.</span>
-                </div>
-                <div  class="follow">
-                    <span>Follow Us on Twitter</span>
-                </div>
-            </div>
-        </div>
-        <div  class="secondery-navigation">
-            <h2  class="menu">Navigation</h2>
-            <ul>
-                <li><a href= "#">Web Development</a></li>
-                <li><a href= "#">Web Apps</a></li>
-                <li><a href= "#">Web Design</a></li>
-                <li><a href= "#">Print Design</a></li>
-                <li><a href= "#">Graphic Design</a></li>
-                <li><a href= "#">Design Partners</a></li>
-                <li><a href= "#">Online Shops</a></li>
-                <li><a href= "#">Online Marketing</a></li>
-            </ul>
-        </div>
-        <div class="contacts">
-            <h2>Contact Us</h2>
-            <ul>
-                <li>E-mail: hello@wooddesign.info</li>
-                <li>Phone: +370 6411 9028</li>
-                <li>Twitter: @wooddesigninfo</li>
-            </ul>
-        </div>
-    </aside>
+
+    <?php get_sidebar(); ?>
+
     <?php get_footer(); ?>
 
